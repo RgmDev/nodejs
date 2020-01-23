8129cf40df329b5e0ab5858650373c98d4db17ee
Rubén Gonzaléz <rgonzalez@MKD.car>
2020-01-23 13:36:26 +0100

    Añadido http.js - web server

---
+++ http.js
@@ -0,0 +1,18 @@
+var http = require('http'); // Web Server
+
+http.createServer(function (req, res) {
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  res.write('<h1>Hello World!</h1>');
+  res.write('<h1>URL: '+req.url+'</h1>'); 
+
+  var base = 'http://' + req.headers.host + '/';
+  var url = new URL(req.url, base);
+  var txt = url.port+' '+url.searchParams.get('year')+'/'+url.searchParams.get('month');
+
+  res.end(txt);
+
+}).listen(8080);
+
+// http://localhost:8080/summer?year=2020&month=October
