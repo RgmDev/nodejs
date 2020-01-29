b128884f19d6be86c1c4488c0507cfa6ad93e30b
Rubén Gonzaléz <rgonzalez@MKD.car>
2020-01-29 17:02:25 +0100

    Añadido PM2

--- pm2.js
+++ pm2.js
@@ -13,11 +13,11 @@
   https://riptutorial.com/es/node-js/example/21325/despliegue-utilizando-pm2
 
 */
-
-var http = require('http');
+const http = require('http'); // Web Server
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    return res.end(new Date());
-  }
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<h1>PM2 is running!</h1>');
+  let fecha = new Date();
+  res.end('Timestamp: '+fecha.getDate()+'/'+fecha.getMonth()+'/'+fecha.getFullYear()+' '+fecha.getHours()+':'+fecha.getMinutes()+':'+fecha.getSeconds());
 }).listen(8080);
